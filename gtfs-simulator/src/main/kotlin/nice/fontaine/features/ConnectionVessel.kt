c12dd4ee2dfa7fba6cf424c8829095c0a578e9ac
Henrique Faria <henriquenunesfaria@gmail.com>
2019-02-08 17:46:23 +0100

    tmp

--- gtfs-simulator/src/main/kotlin/nice/fontaine/features/ConnectionVessel.kt
+++ gtfs-simulator/src/main/kotlin/nice/fontaine/features/ConnectionVessel.kt
@@ -7,7 +7,6 @@ import org.jxmapviewer.viewer.GeoPosition
 
 class ConnectionVessel(private val handler: MapHandler, private val connection: Connection) {
     private lateinit var stops: MutableList<Stop>
-    private lateinit var path: ConnectionPath
 
     init {
         stops = mutableListOf()
@@ -15,23 +14,14 @@ class ConnectionVessel(private val handler: MapHandler, private val connection: 
             stops.add(it.origin)
             stops.add(it.destination)
         }
-        path = ConnectionPath(stops)
     }
 
     fun update(timestamp: Int) {
-        if (!path.timeRange.contains(timestamp)) return
-        /*if (!path.timeRange.contains(timestamp)) return
-        path.step()
-        val style = Element.Point(ORANGE, 200.0)
-        handler.update("VEHICLE", path.currentPosition(), style)*/
+       //TODO:
     }
 
     fun show() {
         handler.updates("SHAPE", stops.map{it.position}.toMutableList(), Element.Line(GREEN, 4F))
         handler.updates("STOPS", stops.map{it.position}.toMutableList(), Element.Point(BLUE, 50.0))
     }
-
-
-
-
 }
