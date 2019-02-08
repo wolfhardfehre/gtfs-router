9923c877d17e7833885dfde4fd4872ec2f5765a4
Henrique Faria <henriquenunesfaria@gmail.com>
2019-02-08 16:29:43 +0100

    Fix build

--- gtfs-simulator/src/main/kotlin/nice/fontaine/features/ConnectionPath.kt
+++ gtfs-simulator/src/main/kotlin/nice/fontaine/features/ConnectionPath.kt
@@ -1,4 +1,13 @@
 package nice.fontaine.features
 
-class ConnectionPath {
+import nice.fontaine.models.network.Stop
+import org.jxmapviewer.viewer.GeoPosition
+
+class ConnectionPath(val stop: MutableList<Stop>) {
+    val startTime = stop[0].time
+    val endTime = stop[stop.size - 1].time
+    val timeRange = startTime..endTime
+
+
+    val currentPosition: GeoPosition = stop[0].position
 }
--- gtfs-simulator/src/main/kotlin/nice/fontaine/features/ConnectionVessel.kt
+++ gtfs-simulator/src/main/kotlin/nice/fontaine/features/ConnectionVessel.kt
@@ -1,31 +1,37 @@
 package nice.fontaine.features
 
-import nice.fontaine.extensions.toShapePositions
-import nice.fontaine.extensions.toStopPositions
 import nice.fontaine.gui.map.*
-import nice.fontaine.models.additions.Segment
-import nice.fontaine.models.csv.CsvTrip
 import nice.fontaine.models.network.Connection
+import nice.fontaine.models.network.Stop
 import org.jxmapviewer.viewer.GeoPosition
 
 class ConnectionVessel(private val handler: MapHandler, private val connection: Connection) {
-    private val stops: MutableList<GeoPosition> = mutableListOf()
+    private lateinit var stops: MutableList<Stop>
+    private lateinit var path: ConnectionPath
+
     init {
+        stops = mutableListOf()
         connection.legs.forEach {
-            stops.add(it.origin.position)
-            stops.add(it.destination.position)
+            stops.add(it.origin)
+            stops.add(it.destination)
         }
+        path = ConnectionPath(stops)
     }
 
     fun update(timestamp: Int) {
         if (!path.timeRange.contains(timestamp)) return
+        /*if (!path.timeRange.contains(timestamp)) return
         path.step()
         val style = Element.Point(ORANGE, 200.0)
-        handler.update("VEHICLE", path.currentPosition(), style)
+        handler.update("VEHICLE", path.currentPosition(), style)*/
     }
 
     fun show() {
-        handler.updates("SHAPE", stops, Element.Line(GREEN, 4F))
-        handler.updates("STOPS", stops, Element.Point(BLUE, 50.0))
+        handler.updates("SHAPE", stops.map{it.position}.toMutableList(), Element.Line(GREEN, 4F))
+        handler.updates("STOPS", stops.map{it.position}.toMutableList(), Element.Point(BLUE, 50.0))
     }
+
+
+
+
 }
